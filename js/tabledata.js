0307abeefd7faa3235b11adc6bee6314be33b12c
zoomshoes <meman2804@gmail.com>
2023-12-20 22:19:12 -0800

    grabbed table data from json!
    also it's sortable! lfggggggggg

--- js/tabledata.js
+++ js/tabledata.js
@@ -1,30 +1,35 @@
-var url = "data/data.json";
-fetch(url)
-    .then(response => response.json())
-    .then(json => {
-        console.log(json);
-        // Do stuff with the contents of the JSON file here
-
-async function readJSONFile(file) {
-    // Function will return a new Promise which will resolve or reject based on whether the JSON file is read and parsed successfully
-    return new Promise((resolve, reject) => {
-        // Define a FileReader Object to read the file
-        let fileReader = new FileReader();
-        // Specify what the FileReader should do on the successful read of a file
-        fileReader.onload = event => {
-            // If successfully read, resolve the Promise with JSON parsed contents of the file
-            resolve(JSON.parse(event.target.result))
-        };
-        // If the file is not successfully read, reject with the error
-        fileReader.onerror = error => reject(error);
-        // Read from the file, which will kick-off the onload or onerror events defined above based on the outcome
-        fileReader.readAsText(file);
+$(function() {
+    $.getJSON('data/data.json', function(data) {
+        $.each(data.stats, function(i, f) {
+        var tableRow = "<tr>" + 
+            "<td>" + f.Name + "</td>" +
+            "<td>" + f.PTS + "</td>" +
+            "<td>" + f.REB + "</td>" +
+            "<td>" + f.AST + "</td>" +
+            "<td>" + f.FGM + "</td>" +
+            "<td>" + f.FGA + "</td>" +
+            "<td>" + (f.FGP*100).toFixed(1) + "</td>" +
+            "<td>" + f.TS + "</td>" +
+            "</tr>";
+        $(tableRow).appendTo("#statdata tbody");
+        });
     });
-}
-// Function to be triggered when file input changes
-async function fileChange(file){
-    // As readJSONFile is a promise, it must resolve before the contents can be read - in this case logged to the console
-    readJSONFile(file).then(json => console.log(json));
-}
-
 });
+
+// $(function() {
+//     $.getJSON('data/export.json', function(data) {
+//         $.each(data.players, function(i, f) {
+//         var tableRow = "<tr>" + 
+//             "<td>" + f.ast + "</td>" +
+//             "<td>" + f.astp + "</td>" +
+//             "<td>" + f.drb + "</td>" +
+//             "<td>" + f.dws + "</td>" +
+//             "<td>" + f.fg + "</td>" +
+//             "<td>" + f.fga + "</td>" +
+//             // "<td>" + (f.FGP*100).toFixed(1) + "</td>" +
+//             "<td>" + 'a' + "</td>" +
+//             "</tr>";
+//         $(tableRow).appendTo("#statdata tbody");
+//         });
+//     });
+// });
